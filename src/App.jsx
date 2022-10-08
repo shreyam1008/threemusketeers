e67ad983d72abff474173ef03ace4b2a3d36e1ca
Shreyam Adhikari <shreyam1008@gmail.com>
2022-10-08 14:47:52 +0545

    Bug: Fix file name error on index imports.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-import './App.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Layout from './Components/Layout'
-import Home from './Pages/Home'
-import Chat from './Pages/Chat'
-import Quiz from './Pages/Quiz'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Components/Layout";
+import Home from "./Pages/Home";
+import Chat from "./Pages/Chat";
+import Quiz from "./Pages/Quiz";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <div classname="App">
-       <Routes>
-        <Route element={<Layout />}>  //Higher Order Component
-          <Route path='/' element={<Home />}/>
-          <Route path='/chat' element={<Chat />}/>
-          <Route path='/quiz' element={<Quiz />}/>
-        </Route>
-      </Routes>
-    </div>
+      <div classname="App">
+        <Routes>
+          <Route element={<Layout />}>
+            {" "}
+            //Higher Order Component
+            <Route path="/" element={<Home />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/quiz" element={<Quiz />} />
+          </Route>
+        </Routes>
+      </div>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default App;
